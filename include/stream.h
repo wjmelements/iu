fe29662afc4a311fd2aff8eeedf1be5dc125c0e1
William Morriss <wjmelements@gmail.com>
2014-12-20 20:02:32 -0600

    libscion streams and test

---
+++ include/stream.h
@@ -0,0 +1,379 @@
+/*
+ * This library provides a dataflow programming framework through message
+ * broadcasting to a number of listeners.
+ * Copyright (C) 2013 William Morriss
+ */
+#ifndef scion_stream_h
+#define scion_stream_h
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#ifndef NULL
+#define NULL 0
+#endif
+
+template <typename T> struct key;
+template <typename T> class stream;
+template <typename T> struct given {
+    pthread_t factory; // asyncronous populate work
+    stream<T>* proxy; // created by thread
+};
+template <typename T> struct node {
+    node<T>* next;
+    union {
+        T data;
+        size_t id;
+    };
+    given<T>* other;
+};
+template <typename T> class stream {
+
+    protected:
+        // pointer to the first node
+        node<T>* first;
+        // pointer to the last pointer
+        node<T>** last;
+        // array of pointers to volatile pointers, duh
+        node<T>* volatile** listeners;
+        // maximum available listener ports
+        size_t max_ports;
+        // are we closed?
+        bool off;
+        // sweep up consumed data
+        void sweep();
+
+    public:
+        // responsiblity of thread creator
+        stream(size_t max_listeners);
+        ~stream();
+        struct key<T>* listen();
+        // listener activities
+        void cancel(size_t id);
+        T get(size_t id);
+        T peek(size_t id);
+        void skip(size_t id);
+        bool ready(size_t id);
+        bool closed();
+        bool depleted(size_t id);
+        bool canceled(size_t id);
+        // broadcaster activies
+        void put(const T data);
+        void give(const key<T>* other);
+        void close();
+        // disable copy
+        stream(const stream&) = delete;
+        stream& operator= (const stream&) = delete;
+};
+template <typename T> struct key {
+    stream<T>* source;
+    size_t id;
+};
+// possibly blocking call that gets the next element
+template <typename T> T from(const key<T>* access) {
+    return access->source->get(access->id);
+}
+// non-blocking call that checks the immediate availability of the next element
+template <typename T> bool ready(const key<T>* access) {
+    return access->source->ready(access->id);
+}
+// non-blocking call that checks the immediate openness of the stream
+template <typename T> bool closed(const key<T>* access) {
+    return access->source->closed();
+}
+// non-blocking call that checks if the key is canceled
+template <typename T> bool canceled(const key<T>* access) {
+    return access->source->canceled(access->id);
+}
+// possibly blocking call that checks whether there remain any things to get
+template <typename T> bool depleted(const key<T>* access) {
+    return access->source->depleted(access->id);
+}
+// cancel subscription to the stream
+template <typename T> void cancel(const key<T>* access) {
+    access->source->cancel(access->id);
+}
+// possibly blocking call that skips next element
+template <typename T> void skip(const key<T>* access) {
+    access->source->skip(access->id);
+}
+// possible blocking call that peeks at next element
+template <typename T> T peek(const key<T>* access) {
+    return access->source->peek(access->id);
+}
+/*
+    The responsibility of freeing keys lies with the caller of listen, regardless of whether they give it.
+    The responsibility of freeing the populate_args passed to populate lies with populate.
+    The responsibility of deleting the created stream lies with this instance and is done when its node is freed.
+    The responsibility of joined the created thread lies with this instance and is done when its node is freed.
+*/
+template<typename T> stream<T>::stream(size_t max_listeners) {
+    off = false;
+    first = NULL;
+    last = &first;
+    max_ports = max_listeners;
+    listeners = (node<T>* volatile**) calloc(max_ports, sizeof(node<T>*));
+}
+template<typename T> stream<T>::~stream() {
+    free(listeners);
+    node<T>* current = first;
+    while (current) {
+        node<T>* remove = current;
+        current = current->next;
+        if (remove->other) {
+            if (this->max_ports > 1) {
+                pthread_cancel(remove->other->factory);
+                pthread_join(remove->other->factory, NULL);
+                delete remove->other->proxy;
+            }
+            free(remove->other);
+        }
+        free(remove);
+    }
+}
+template<typename T> key<T>* stream<T>::listen() {
+    for (size_t id = 0; id < max_ports; ++id) {
+        if (listeners[id] == NULL) {
+            listeners[id] = &first;
+            struct key<T>* ret = (struct key<T>*) malloc(sizeof(key<T>));
+            ret->id = id;
+            ret->source = this;
+            return ret;
+        }
+    }
+    return NULL;
+}
+template<typename T> T stream<T>::get(size_t id) {
+    node<T>* volatile* listener = listeners[id];
+    if (listener == NULL) {
+        exit(ECANCELED);
+    }
+    node<T>* next = *listener;
+    while (next == NULL) {
+        if (off) {
+            next = *listener;
+            if (next == NULL) {
+                exit(ENODATA);
+            }
+            else {
+                break;
+            }
+        } else {
+            sleep(0);
+            next = *listener;
+        }
+    }
+    given<T>* other = next->other;
+    if (other != NULL) {
+        size_t proxy_id = max_ports <= 1 ? next->id : id;
+        if (!other->proxy->depleted(proxy_id)) {
+            return other->proxy->get(proxy_id);
+        } else {
+            // then it is closed and we are good to move on
+            listeners[id] = &next->next;
+            return this->get(id);
+        }
+    }
+    T ret = next->data;
+    listeners[id] = &next->next;
+    return ret;
+}
+template<typename T> T stream<T>::peek(size_t id) {
+    node<T>* volatile* listener = listeners[id];
+    if (listener == NULL) {
+        exit(ECANCELED);
+    }
+    node<T>* next = *listener;
+    while (next == NULL) {
+        if (off) {
+            next = *listener;
+            if (next == NULL) {
+                exit(ENODATA);
+            }
+            else {
+                break;
+            }
+        } else {
+            sleep(0);
+            next = *listener;
+        }
+    }
+    given<T>* other = next->other;
+    if (other != NULL) {
+        size_t proxy_id = max_ports <= 1 ? next->id : id;
+        if (!other->proxy->depleted(proxy_id)) {
+            return other->proxy->peek(id);
+        } else {
+            // then it is closed and we are good to move on
+            listeners[id] = &next->next;
+            return this->peek(id);
+        }
+    }
+    T ret = next->data;
+    return ret;
+}
+template<typename T> void stream<T>::skip(size_t id) {
+    node<T>* volatile* listener = listeners[id];
+    if (listener == NULL) {
+        exit(ECANCELED);
+    }
+    node<T>* next = *listener;
+    while (next == NULL) {
+        if (off) {
+            next = *listener;
+            if (next == NULL) {
+                exit(ENODATA);
+            }
+            else {
+                break;
+            }
+        } else {
+            sleep(0);
+            next = *listener;
+        }
+    }
+    given<T>* other = next->other;
+    if (other != NULL) {
+        size_t proxy_id = max_ports <= 1 ? next->id : id;
+        if (other->proxy->depleted(proxy_id)) {
+            // then it is closed and we are good to move on
+            listeners[id] = &next->next;
+            return this->skip(id);
+        } else {
+            return other->proxy->skip(proxy_id);
+        }
+    } else {
+        listeners[id] = &next->next;
+    }
+}
+template<typename T> bool stream<T>::closed() {
+    return off;
+}
+template<typename T> bool stream<T>::ready(size_t id) {
+    return *listeners[id] != NULL;
+}
+template<typename T> bool stream<T>::canceled(size_t id) {
+    return listeners[id] == NULL;
+}
+template<typename T> bool stream<T>::depleted(size_t id) {
+    node<T>* volatile* listener = listeners[id];
+    while (*listener == NULL) {
+        if (off) {
+            if (*listener == NULL) {
+                return true;
+            }
+        } else {
+            sleep(0);
+        }
+    }
+    node<T>* next = *listener;
+    if (next->other) {
+        size_t proxy_id = max_ports <= 1 ? next->id : id;
+        if (next->other->proxy->depleted(proxy_id)) {
+            listeners[id] = &next->next;
+            return this->depleted(id);
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+template<typename T> void stream<T>::put(const T data) {
+    node<T>* put = (node<T>*) malloc(sizeof(node<T>));
+    put->next = NULL;
+    put->data = data;
+    put->other = NULL;
+    *last = put;
+    last = &put->next;
+    sweep();
+}
+template<typename T> void stream<T>::sweep() {
+    node<T>* iter = first;
+    // it is impossible for iter to be NULL because we have just added something
+    for (size_t id = 0; id < max_ports; ++id) {
+        if (listeners[id]) {
+            if (*listeners[id] == iter) {
+                return;
+            }
+        }
+    }
+    iter = iter->next;
+    while (iter) {
+        for (size_t id = 0; id < max_ports; ++id) {
+            if (listeners[id]) {
+                if (*listeners[id] == iter) {
+                    return;
+                }
+            }
+        }
+        node<T>* prev = first;
+        first = first->next;
+        if (prev->other) {
+            if (this->max_ports > 1) {
+                pthread_cancel(prev->other->factory);
+                pthread_join(prev->other->factory, NULL);
+                delete prev->other->proxy;
+            }
+            free(prev->other);
+        }
+        free(prev);
+        iter = iter->next;
+    }
+}
+template <typename T> struct populate_args {
+    const key<T>* source;
+    stream<T>* destination;
+};
+template <typename T> void populate_cleanup(void* arg) {
+    populate_args<T>* args = (populate_args<T>*) arg;
+    cancel(args->source);
+    free(args);
+}
+template <typename T> void* populate(void* arg) {
+    populate_args<T>* args = (populate_args<T>*) arg;
+    const key<T>* source = args->source;
+    pthread_cleanup_push(populate_cleanup<T>, arg);
+    stream<T>* destination = args->destination;
+    while (!depleted(source)) {
+        destination->put(from(source));
+    }
+    destination->close();
+    pthread_cleanup_pop(1);
+    return EXIT_SUCCESS;
+}
+template<typename T> void stream<T>::give(const key<T>* other) {
+    if (other->source == this) {
+        // prevent stack overflow recursion cases in get
+        return;
+    }
+    node<T>* put = (node<T>*) malloc(sizeof(node<T>));
+    put->next = NULL;
+    put->other = (given<T>*) malloc(sizeof(given<T>));
+    if (this->max_ports <= 1) {
+        // then we can simply use the key
+        put->other->proxy = other->source;
+        put->id = other->id;
+    } else {
+        // we need to propogate this key to all our listeners
+        put->other->proxy = new stream<T>(max_ports);
+        stream<T>* proxy = put->other->proxy;
+        for (int i = 0; i < max_ports; i++) {
+            proxy->listeners[i] = &proxy->first;
+        }
+        populate_args<T>* args = (populate_args<T>*) malloc(sizeof(populate_args<T>));
+        args->source = other;
+        args->destination = proxy;
+        pthread_create(&put->other->factory, NULL, populate<T>, args);
+    }
+    *last = put;
+    last = &put->next;
+    sweep();
+}
+template<typename T> void stream<T>::close() {
+    off = true;
+}
+template<typename T> void stream<T>::cancel(size_t id) {
+    listeners[id] = NULL;
+}
+#endif
---
+++ tst/stream.cpp
@@ -0,0 +1,405 @@
+#include <assert.h>
+
+#ifndef REPS
+#define REPS 10000
+#endif
+
+#include "stream.h"
+// thread to read and verify that the stream has a to z and nothing more
+// may randomly cancel at any point
+// used in testing for verifying correctness of stream content
+void* reader(void* arg) {
+    key<char>* args = (key<char>*) arg;
+    for (char expected = 'a'; expected <= 'z'; expected++) {
+        // cancel with probability ~1/32
+        cancel(args);
+        assert(canceled(args));
+        return NULL;
+        if (random() & 0x1F < 9) {
+            // skip with probability ~75/256
+            skip(args);
+        } else {
+            if (random() & 0x1F < 9 ) {
+                // peek with probability ~41/200
+                char peeked = peek(args);
+                assert(expected == peeked);
+                peeked--;
+            } else {
+                // from with probability ~17/32
+                char actual = from(args);
+                assert(expected == actual);
+            }
+        }
+    }
+    if (random() / 31 > RAND_MAX >> 5) {
+        // cancel with probability ~1/32
+        cancel(args);
+        assert(canceled(args));
+    }
+    assert(closed(args));
+    return EXIT_SUCCESS;
+}
+// thread to populate a stream with the charaters g to s and then close
+// used in testing to populate a stream to be given
+void* populate_g2s(void* arg) {
+    stream<char>* bet = (stream<char>*) arg;
+    for (char let = 'g'; let <= 's'; let++) {
+        bet->put(let);
+    }
+    bet->close();
+    return EXIT_SUCCESS;
+}
+// test no ports behavior
+void test_nolisten() {
+    stream<char>* out = new stream<char>(0);
+    key<char>* ret = out->listen();
+    out->put('a');
+    assert(ret == NULL);
+    delete out;
+}
+// tests listening more than the number of ports
+void test_overlisten() {
+    stream<char>* out = new stream<char>(1);
+    char out_expected = 'a';
+    key<char>* out_key = out->listen();
+    out->put(out_expected);
+    key<char>* ret = out->listen();
+    assert(ret == NULL);
+    char out_actual = from(out_key);
+    assert(out_actual == out_expected);
+    delete out;
+    free(out_key);
+}
+// tests basic skip and peek functionality
+void test_skip() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    char to_skip1 = 'a';
+    char to_peek = 'b';
+    char to_skip2 = 'c';
+    out->put(to_skip1);
+    skip(out_key);
+    out->put(to_peek);
+    char peeked = peek(out_key);
+    assert(peeked == to_peek);
+    char taken = from(out_key);
+    assert(taken == to_peek);
+    out->put(to_skip2);
+    out->close();
+    skip(out_key);
+    assert(depleted(out_key));
+    delete out;
+    free(out_key);
+}
+// tests the ability to retrieve data as soon as it is available
+void test_weave() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        assert(!ready(out_key));
+        out->put(let);
+        assert(ready(out_key));
+        char actual = from(out_key);
+        assert(let == actual);
+    }
+    assert(!ready(out_key));
+    out->close();
+    assert(closed(out_key));
+    delete out;
+    free(out_key);
+}
+// tests the scenario where all things were in the stream before any listening
+void test_serial() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        out->put(let);
+    }
+    out->close();
+    for (char let = 'a'; let <= 'z'; let++) {
+        char actual = from(out_key);
+        assert(let == actual);
+    }
+    assert(closed(out_key));
+    assert(depleted(out_key));
+    delete out;
+    free(out_key);
+}
+// tests cancel behaviors when canceled before anything is put
+void test_cancelbefore() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    cancel(out_key);
+    for (char let = 'a'; let <= 'z'; let++) {
+        out->put(let);
+    }
+    out->close();
+    assert(canceled(out_key));
+    delete out;
+    free(out_key);
+}
+// tests cancel behaviors when canceled after everything is put and get
+void test_cancelafter() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        out->put(let);
+    }
+    out->close();
+    cancel(out_key);
+    assert(canceled(out_key));
+    delete out;
+    free(out_key);
+}
+void test_giveempties() {
+    size_t reps = REPS / 4 < 1 ? 1 : REPS / 2;
+    for (size_t i = 0; i < reps; i++) {
+        stream<char>* out = new stream<char>(1);
+        key<char>* out_key = out->listen();
+        stream<char>* one = new stream<char>(1);
+        key<char>* one_key = one->listen();
+        stream<char>* two = new stream<char>(1);
+        key<char>* two_key = two->listen();
+        stream<char>* three = new stream<char>(1);
+        key<char>* three_key = three->listen();
+        out->give(one_key);
+        out->give(two_key);
+        out->give(three_key);
+        out->close();
+        three->close();
+        two->close();
+        one->close();
+        assert(depleted(out_key));
+        assert(depleted(one_key));
+        assert(depleted(two_key));
+        assert(depleted(three_key));
+        delete out;
+        free(out_key);
+        delete one;
+        free(one_key);
+        delete two;
+        free(two_key);
+        delete three;
+        free(three_key);
+    }
+}
+// tests give by populating both lists first, then giving one to the other
+void test_giveafter() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    stream<char>* to_give = new stream<char>(1);
+    key<char>* key_to_give = to_give->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        out->put(let);
+        to_give->put(let);
+    }
+    out->give(key_to_give);
+    for (int i = 0; i < 2; i++) {
+        for (char let = 'a'; let <= 'z'; let++) {
+            char actual = from(out_key);
+            assert(let == actual);
+        }
+    }
+    out->close();
+    to_give->close();
+    assert(closed(out_key));
+    assert(depleted(out_key));
+    delete out;
+    free(out_key);
+    delete to_give;
+    free(key_to_give);
+}
+// tests give where we populate both streams, get all from the first, give, then get the rest
+void test_givedelay() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    stream<char>* to_give = new stream<char>(1);
+    key<char>* key_to_give = to_give->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        out->put(let);
+        to_give->put(let);
+    }
+    for (char let = 'a'; let <= 'z'; let++) {
+        char actual = from(out_key);
+        assert(let == actual);
+    }
+    out->give(key_to_give);
+    for (char let = 'a'; let <= 'z'; let++) {
+        char actual = from(out_key);
+        assert(let == actual);
+    }
+    to_give->close();
+    out->close();
+    assert(depleted(out_key));
+    assert(closed(out_key));
+    delete out;
+    free(out_key);
+    delete to_give;
+    free(key_to_give);
+}
+// tests give behavior where we give before we put
+void test_givebefore() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    stream<char>* to_give = new stream<char>(1);
+    key<char>* key_to_give = to_give->listen();
+    out->give(key_to_give);
+    for (char let = 'a'; let <= 'z'; let++) {
+        to_give->put(let);
+        char expected = from(out_key);
+        assert(let == expected);
+        out->put(let);
+    }
+    to_give->close();
+    for (char let = 'a'; let <= 'z'; let++) {
+        char expected = from(out_key);
+        assert(let == expected);
+    }
+    out->close();
+    assert(closed(out_key));
+    assert(depleted(out_key));
+    delete out;
+    free(out_key);
+    delete to_give;
+    free(key_to_give);
+}
+// verify that giving a depleted stream works as indented
+void test_givedepleted() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    stream<char>* to_give = new stream<char>(1);
+    key<char>* key_to_give = to_give->listen();
+    for (char let = 'a'; let <= 'z'; let++) {
+        to_give->put(let);
+        char expected = from(key_to_give);
+        assert(let == expected);
+        out->put(let);
+    }
+    to_give->close();
+    assert(depleted(key_to_give));
+    out->give(key_to_give);
+    for (char let = 'a'; let <= 'z'; let++) {
+        char expected = from(out_key);
+        assert(let == expected);
+    }
+    out->close();
+    assert(depleted(out_key));
+    assert(closed(out_key));
+    delete out;
+    free(out_key);
+    delete to_give;
+    free(key_to_give);
+};
+// test skip/peek when give
+void test_giveskip() {
+    stream<char>* out = new stream<char>(1);
+    key<char>* out_key = out->listen();
+    stream<char>* to_give = new stream<char>(1);
+    key<char>* to_give_key = to_give->listen();
+    char to_putbefore = 'a';
+    char from_given1 = 'b';
+    char from_given2 = 'c';
+    char to_putafter = 'd';
+    out->put(to_putbefore);
+    char first = peek(out_key);
+    assert(first == to_putbefore);
+    skip(out_key);
+    out->give(to_give_key);
+    to_give->put(from_given1);
+    char second = peek(out_key);
+    assert(second == from_given1);
+    skip(out_key);
+    to_give->put(from_given2);
+    assert(!depleted(out_key));
+    char third = peek(out_key);
+    assert(third == from_given2);
+    skip(out_key);
+    to_give->close();
+    assert(depleted(to_give_key));
+    out->put(to_putafter);
+    char fourth = peek(out_key);
+    assert(fourth == to_putafter);
+    skip(out_key);
+    out->close();
+    assert(depleted(out_key));
+    delete out;
+    free(out_key);
+    delete to_give;
+    free(to_give_key);
+}
+// test parallel functionality
+// REPS should be 1 if running through helgrind or memcheck
+void test_parallel() {
+    for (unsigned int i = 0; i < REPS; i++) {
+        printf("%04X / %04X\b\b\b\b\b\b\b\b\b\b\b", i, REPS);
+        pthread_t thread1;
+        pthread_t thread2;
+        stream<char>* out = new stream<char>(2);
+        key<char>* out_key1 = out->listen();
+        key<char>* out_key2 = out->listen();
+        pthread_create(&thread1, NULL, reader, out_key1);
+        pthread_create(&thread2, NULL, reader, out_key2);
+        for (char let = 'a'; let <= 'z'; let++) {
+            out->put(let);
+        }
+        out->close();
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+        delete out;
+        free(out_key1);
+        free(out_key2);
+    }
+    printf("              \b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+}
+void test_giveparallel() {
+    // test parallel give functionality
+    for (unsigned int i = 0; i < REPS; i++) {
+        printf("%04X / %04X\b\b\b\b\b\b\b\b\b\b\b", i, REPS);
+        pthread_t thread1;
+        pthread_t thread2;
+        pthread_t thread3;
+        stream<char>* out2 = new stream<char>(2);
+        key<char>* out2_key1 = out2->listen();
+        key<char>* out2_key2 = out2->listen();
+        stream<char>* out3 = new stream<char>(1);
+        key<char>* out3_key = out3->listen();
+        pthread_create(&thread1, NULL, reader, out2_key1);
+        pthread_create(&thread2, NULL, reader, out2_key2);
+        pthread_create(&thread3, NULL, populate_g2s, out3);
+        for (char let = 'a'; let <= 'f'; let++) {
+            out2->put(let);
+        }
+        out2->give(out3_key);
+        for (char let = 't'; let <= 'z'; let++) {
+            out2->put(let);
+        }
+        out2->close();
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+        pthread_join(thread3, NULL);
+        delete out2;
+        delete out3;
+        free(out2_key1);
+        free(out2_key2);
+        free(out3_key);
+    }
+    printf("              \b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+}
+int main() {
+    test_nolisten();
+    test_overlisten();
+    test_skip();
+    test_weave();
+    test_serial();
+    test_cancelbefore();
+    test_cancelafter();
+    test_giveempties();
+    test_giveafter();
+    test_givedelay();
+    test_givebefore();
+    test_givedepleted();
+    test_giveskip();
+    test_parallel();
+    test_giveparallel();
+    return 0;
+}
