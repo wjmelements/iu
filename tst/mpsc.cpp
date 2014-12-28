f2496e5ec968cd7e0cbdc0ffc44e8115505faa7f
William Morriss <wjmelements@gmail.com>
2014-12-28 02:48:48 -0600

    verify sequential consistency

--- tst/mpsc.cpp
+++ tst/mpsc.cpp
@@ -5,6 +5,7 @@
 
 #include <map>
 using std::map;
+using std::pair;
 
 
 void basic_test() {
@@ -55,13 +56,56 @@ void para_test() {
     }
     assert(counts.size() == NITEMS);
 }
+struct worker_arg {
+    int id;
+    mpsc<pair<int, int> >* q;
+};
+void* seq_cons_test_worker(void* arg) {
+    struct worker_arg* warg = (worker_arg*) arg;
+    int id = warg->id;
+    mpsc<pair<int, int> >* q = warg->q;
+    free(warg);
+    for (int i = 0; i < NITEMS; i++) {
+        q->put(pair<int, int>(id, i));
+    }
+    return NULL;
+}
+void seq_cons_test() {
+    mpsc<pair<int, int> > q;
+    map<int, int> counts;
+    for (int i = 0; i < NTHREADS; i++) {
+        struct worker_arg* warg = (struct worker_arg*) Malloc(sizeof(worker_arg));
+        warg->id = i;
+        warg->q = &q;
+        pthread_t thread;
+        Pthread_create(&thread, NULL, seq_cons_test_worker, warg);
+        Pthread_detach(thread);
+        counts[i] = 0;
+    }
+    do {
+        const pair<int, int>* got = q.get();
+        if (got) {
+            assert(counts[got->first] == got->second);
+            if (++counts[got->first] == NITEMS) {
+                counts.erase(got->first);
+            }
+        } else {
+            sched_yield();
+        }
+    } while (counts.size());
+}
 int main() {
     basic_test();
-    #define NTRIALS 5000
+    #define NTRIALS 8000
     for (size_t i = 0; i < NTRIALS; i++) {
         printf("%04X / %04X\b\b\b\b\b\b\b\b\b\b\b", i, NTRIALS);
-        para_test();
-    }
+        if (i < NTRIALS / 2) {
+            para_test();
+        } else {
+            seq_cons_test();
+        }
+    }       
     printf("              \b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+    // verify sequential consistency
     return 0;
 }
